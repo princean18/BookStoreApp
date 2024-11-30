a44780d354ec8994690937969a8e338e69868ec3
Prince <princepandrews@gmail.com>
2024-11-30 14:46:26 +0000

    Setup with basic css and js files for template

--- PrintOrder/Program.cs
+++ PrintOrder/Program.cs
@@ -24,4 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(name: "Dashboard",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+
 app.Run();
+
+
